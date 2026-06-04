7f7023daa9d93b54e2a23545a0283fb8e5bb80f6
omprakashpatna18-del <om.prakashpatna18@gmail.com>
2026-06-04 21:26:04 +0530

    Update ai_chat.py

--- utils/ai_chat.py
+++ utils/ai_chat.py
@@ -12,7 +12,7 @@ def get_ai_reply(message):
         res = client.chat.completions.create(
             model="llama-3.1-8b-instant",
             messages=[
-                {"role": "system", "content": "You are a financial advisor for India."},
+                {"role": "system", "content": "You are a financial advisor for India. "},
                 {"role": "user", "content": message}
             ]
         )
